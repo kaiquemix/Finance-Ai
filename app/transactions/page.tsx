8237fe2a6098bbb5264954c6e9f32968b61cbbe3
kaique-ferreira <kaiquerbn@gmail.com>
2025-02-19 16:02:23 -0300

    chore: add tailwind prettier plugin

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,9 @@
 const Transactions = () => {
-  return <h1>Transactions Pa2ge</h1>;
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-6 text-red-500">Transactions Pa2ge</h1>
+    </div>
+  );
 };
 
 export default Transactions;
